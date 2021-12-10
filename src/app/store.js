422406a06da192de609051f39aef6ff909995586
Dinesh Chandran <dineshchandran311@gmail.com>
2021-12-10 11:08:31 +0530

    Home page

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import userReducer from "../features/userSlice";
+import movieReducer from "../features/movieSlice";
 
 export default configureStore({
   reducer: {
     user: userReducer,
+    movie: movieReducer
   },
   middleware: getDefaultMiddleware({
     serializableCheck: false,
